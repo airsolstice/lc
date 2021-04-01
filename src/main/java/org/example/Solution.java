4cc528eb54d6584bbd22284b42e3aa5cceb2a7d8
linxibiao <linxibiao@meituan.com>
2021-04-01 17:15:20 +0800

    f

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -8,63 +8,80 @@ class Solution {
         Solution solution = new Solution();
 
         char[][] cube = new char[][]{
-                {'5','3','.','.','7','.','.','.','.'},
-                {'6','.','.','1','9','5','.','.','.'},
-                {'.','9','8','.','.','.','.','6','.'},
-                {'8','.','.','.','6','.','.','.','3'},
-                {'4','.','.','8','.','3','.','.','1'},
-                {'7','.','.','.','2','.','.','.','6'},
-                {'.','6','.','.','.','.','2','8','.'},
-                {'.','.','.','4','1','9','.','.','5'},
-                {'.','.','.','.','8','.','.','7','9'}
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
         };
 
-
-        System.out.println(solution.isValidSudoku(cube));
-
+        int[][] martix = new int[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+        solution.rotate(martix);
+        for (int i = 0; i < martix.length; i++) {
+            for (int i1 = 0; i1 < martix[i].length; i1++) {
+                System.out.print(martix[i][i1] + ",");
+            }
+            System.out.println();
+        }
+//        System.out.println(solution.isValidSudoku(cube));
 //        solution.moveZeroes(new int[]{0,1,0,3,12});
 //        solution.moveZeroes(new int[]{1,0});
 //        System.out.println(Arrays.toString(solution.twoSum1(new int[]{1, 3, 4, 4}, 8)));
 //        System.out.println(solution.binarySearch(new int[]{1, 2, 7,11, 15}, 1, 0, 5));
     }
 
-    public boolean isValidSudoku(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            Set<Character> r = new HashSet<>();
-            Set<Character> c = new HashSet<>();
-            Set<Character> m = new HashSet<>();
-            for (int j = 0; j < board[i].length; j++) {
-                if(!".".equals(board[i][j] + "") &&r.contains(board[i][j])){
-                    return false;
-                } else {
-                    r.add(board[i][j]);
-                }
 
-                if(!".".equals(board[j][i] + "") && c.contains(board[j][i])){
-                    return false;
-                } else {
-                    c.add(board[j][i]);
-                }
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
 
-                if(i % 3 ==0 && j %3 == 0){
-                    for (int k = 0; k < 3; k++) {
-                        for (int l = 0; l < 3; l++) {
-                            if(!".".equals(board[i + k][j + l] + "") && m.contains(board[i + k][j + l])){
-                                return false;
-                            } else {
-                                m.add(board[i + k][j + l]);
-                            }
-                        }
-                    }
-                    m.clear();
-                }
+        for (int i = 0; i < 2; i++) {
+            for (int j = i; j < matrix.length - 1 - i; j++) {
+                int t1 = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                int t2 = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = t1;
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = t2;
             }
         }
+    }
 
+    public boolean isValidSudoku(char[][] board) {
+        int[][] rol = new int[9][10];
+        int[][] col = new int[9][10];
+        int[][] box = new int[9][10];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                int num = board[i][j] - '0';
+                if (rol[i][num] == 1) {
+                    return false;
+                }
+                if (col[j][num] == 1) {
+                    return false;
+                }
+                if (box[(i / 3) * 3 + j / 3][num] == 1) {
+                    return false;
+                }
+                rol[i][num] = 1;
+                col[j][num] = 1;
+                box[(i / 3) * 3 + j / 3][num] = 1;
+            }
+        }
         return true;
 
     }
-
 
 
     public int[] twoSum1(int[] numbers, int target) {
@@ -73,9 +90,9 @@ class Solution {
             int v2 = target - v1;
 
             int idx = binarySearch(numbers, v2, 0, numbers.length);
-            if(idx >= 0){
-                if(i == idx) {
-                    idx ++;
+            if (idx >= 0) {
+                if (i == idx) {
+                    idx++;
                 }
                 return new int[]{i + 1, idx + 1};
             }
@@ -87,15 +104,15 @@ class Solution {
 
     private int binarySearch(int[] numbers, int v, int l, int r) {
 
-        if(l >= r){
+        if (l >= r) {
             return -1;
         }
 
         int mid = (l + r) / 2;
-        if(numbers[mid] == v){
+        if (numbers[mid] == v) {
             return mid;
-        } else if(numbers[mid] > v){
-           return binarySearch(numbers, v, l, mid);
+        } else if (numbers[mid] > v) {
+            return binarySearch(numbers, v, l, mid);
         } else {
             return binarySearch(numbers, v, mid + 1, r);
         }
@@ -105,7 +122,7 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < numbers.length; i++) {
             int v = target - numbers[i];
-            if(map.containsKey(v)){
+            if (map.containsKey(v)) {
                 return new int[]{map.get(v), i};
             } else {
                 map.put(numbers[i], i);
@@ -118,16 +135,16 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int p1 = 0, p2 = 0;
 
-        if(nums.length == 1){
+        if (nums.length == 1) {
             return;
         }
 
         while (p2 < nums.length) {
-            if(nums[p2] != 0){
+            if (nums[p2] != 0) {
                 nums[p1] = nums[p2];
                 p1++;
             }
-            p2 ++;
+            p2++;
         }
 
         while (p1 < nums.length) {
